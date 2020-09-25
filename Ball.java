3023b6e87208618367381c9d65d885fe718f6547
oda0727 <134.tamo@gmail.com>
2020-09-25 20:35:51 +0900

    second

--- Ball.java
+++ Ball.java
@@ -1,20 +1,1 @@
-package object3Kadai2;
-
-public class Ball{
-	//半径の変数radiusをprivateで宣言
-	private double radius;
-	//コンストラクタ
-	Ball(double radius){
-		this.radius = radius;
-	}
-
-	//球の表面積メソッド
-	public double volume() {
-		return(4.0 / 3.0 * Math.PI * Math.pow(this.radius, 3.0));
-	}
-
-	//球の体積メソッド
-	public double areaOfSurface() {
-		return(4.0 * Math.PI * Math.pow(this.radius, 2.0));
-	}
-}
+package object3Kadai2;public class Ball{//////////////////////////////////////////////////////////////////	//半径の変数radiusをprivateで宣言	private double radius;	//コンストラクタ	Ball(double radius){		this.radius = radius;	}	//球の表面積メソッド	public double volume() {		return(4.0 / 3.0 * Math.PI * Math.pow(this.radius, 3.0));	}	//球の体積メソッド	public double areaOfSurface() {		return(4.0 * Math.PI * Math.pow(this.radius, 2.0));	}}
